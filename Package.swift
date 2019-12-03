5ed7033729e99a347447b087855d8eef28be8e66
e-sung <dev.esung@gmail.com>
2019-12-03 19:53:34 +0900

    feat: add Swift Package Manifest

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+    name: "SDRemoteImageView",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(name: "SDRemoteImageView", targets: ["SDRemoteImageView"]),
+    ],
+    targets: [
+        .target(
+            name: "SDRemoteImageView",
+            path: "SDRemoteImageView/Classes"),
+        .testTarget(
+            name: "SDRemoteImageViewTests",
+            dependencies: ["SDRemoteImageView"],
+            path: "Example/Tests")
+    ]
+)

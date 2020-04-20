b790cd8e260717e3bfc197a3f57c88672a3d3b74
Sungdoo Yoo <dev.esung@gmail.com>
2020-04-20 10:52:04 +0900

    chore: remove unncessary commnets

--- SDRemoteImageView/Classes/RemoteImageDownloadable.swift
+++ SDRemoteImageView/Classes/RemoteImageDownloadable.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-/// Wrapper for Kingfisher compatible types. This type provides an extension point for
-/// connivence methods in Kingfisher.
 public struct SDRemoteImageWrapper<Base> {
     public let base: Base
     public init(_ base: Base) {
@@ -16,12 +14,9 @@ public struct SDRemoteImageWrapper<Base> {
     }
 }
 
-/// Represents an object type that is compatible with Kingfisher. You can use `kf` property to get a
-/// value in the namespace of Kingfisher.
 public protocol RemoteImageDownloadable: AnyObject { }
 
 public extension RemoteImageDownloadable {
-    /// Gets a namespace holder for Kingfisher compatible types.
     var sd: SDRemoteImageWrapper<Self> {
         get { return SDRemoteImageWrapper(self) }
         set { }
